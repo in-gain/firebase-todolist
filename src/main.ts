0baaecfa2995aed65c4c9562cd7fbd717529b5b4
in-gain <online_game_kudryavka@yahoo.co.jp>
2020-09-30 05:11:30 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,23 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = <any>{
+  fetch: () => {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach((element: { id: any } , index: any) => {
+      element.id = index
+    });
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save:(todos: any) => {
+    localStorage.setItem(STORAGE_KEY , JSON.stringify(todos))
+  },
+}
+
 const app = new Vue({
   el :'#app',
   data:{
